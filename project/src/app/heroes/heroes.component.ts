a2636a030bed14a00dd835f722a153ba128817bf
Juan Manuel Gentili <gentilijuanmanuel80974a@gmail.com>
2018-01-14 01:40:18 -0300

    Services

--- project/src/app/heroes/heroes.component.ts
+++ project/src/app/heroes/heroes.component.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { Hero } from '../hero';
-import { HEROES } from '../mock-heroes';
+import { HeroService } from '../hero.service';
 
 @Component({
   selector: 'app-heroes',
@@ -8,21 +8,27 @@ import { HEROES } from '../mock-heroes';
   styleUrls: ['./heroes.component.css']
 })
 export class HeroesComponent implements OnInit {
-  hero: Hero = {
-    id: 1,
-    name: "Windstorm"
-  }
-
-  heroes = HEROES;
+  heroes: Hero[];
 
   selectedHero: Hero;
 
-  constructor() { }
+  constructor(private heroService: HeroService) {
+    //generalmente, en el constructor no debe haber más que asignaciones de parámetros a propiedades.
+    //si se quiere traer datos de un server, hacerlo en ngOnInit().
+   }
 
   ngOnInit() {
+    this.getHeroes();
   }
 
   onSelect(hero: Hero) {
     this.selectedHero = hero;
+  }
+
+  //esta versión de getHeroes() espera a tener el observable para emitir el arreglo de héroes.
+  //es una petición asincrónica, en orden al modo de operar de los servidores.
+  getHeroes(): void {
+    this.heroService.getHeroes()
+                    .subscribe(heroes => this.heroes = heroes);
   }
 }
